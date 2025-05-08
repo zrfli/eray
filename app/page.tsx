f2a8a6f329baa15dd21eea68f1328c16c9008833
Eray <78818724+zrfli@users.noreply.github.com>
2025-05-08 09:19:17 +0300

    new

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import research from "@/hooks/research.json"
 import ach from "@/hooks/ach.json"
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import InstagramCard from "@/components/Instagram";
 
 export default function Main() {
   return (
@@ -142,33 +143,12 @@ export default function Main() {
               </div>
           </div>
       </section>
-      <section aria-label="apply" className="py-6">
-        <div className="px-4 mx-auto max-w-screen-xl">
-            <div className="grid items-center gap-8 lg:grid-cols-12 lg:gap-12">
-            <div className="col-span-6 text-center lg:text-left">
-                <h2 className="mb-4 text-4xl font-extrabold leading-tight tracking-tight text-black dark:text-white md:text-5xl lg:text-6xl underline">Apply Nişantaşı</h2>
-                <p className="mb-4 max-w-xl text-gray-500 dark:text-gray-400">Gelecek için bir adım...</p>
-                <div className="flex items-center justify-center space-x-4 lg:justify-start">
-                <Link prefetch={true} href="apply" rel="noopener noreferrer" className="text-sm font-medium underline underline-offset-4 text-black dark:text-gray-300 hover:no-underline">Apply Nedir?</Link>
-                <div className="border-r border-gray-300 dark:border-neutral-700 h-10"></div>
-                <Link prefetch={true} href="apply" rel="noopener noreferrer" className="inline-flex items-center gap-1 text-sm font-medium underline underline-offset-4 text-black dark:text-gray-300 hover:no-underline"> Başvuru Yap <svg width="7" viewBox="0 0 6 6" height="7" aria-hidden="true">
-                    <path fill="currentColor" d="m1.252 5.547-.63-.63 3.16-3.161H1.383L1.39.891h3.887v3.89h-.87l.005-2.396z"></path>
-                </svg>
-                </Link>
-                </div>
-            </div>
-            <div className="col-span-6 hidden md:flex">
-                <video id="applyVideoContent" className="mx-auto h-64 w-full max-w-xl sm:h-96" autoPlay muted loop playsInline preload="none">
-                    <source src="/207fa6df-2d62-4f2a-ad52-769580e89e38.webm" type="video/webm" />
-                </video>
-            </div>
-            </div>
-        </div>
-      </section>
+      <InstagramCard />
       <section aria-label="events" className="py-12">
         <div className="px-4 mx-auto max-w-screen-xl">
             <div className="flex items-center justify-between mb-8">
             <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl dark:text-white">Etkinlikler</h1>
+            <Link prefetch={true} href="/events" rel="noopener noreferrer" className="inline-flex items-center font-medium underline underline-offset-4 text-black dark:text-gray-300 hover:no-underline">Tümünü gör</Link>
             </div>
             <div id="eventsContent" className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
             {events.map((data, id) => (
@@ -192,7 +172,8 @@ export default function Main() {
         <div className="px-4 mx-auto max-w-screen-xl">
             <div className="flex items-center justify-between mb-8">
             <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl dark:text-white">Araştırmalar</h1>
-           </div>
+            <Link prefetch={true} href="/research" rel="noopener noreferrer" className="inline-flex items-center font-medium underline underline-offset-4 text-black dark:text-gray-300 hover:no-underline">Tümünü gör</Link>
+            </div>
             <div id="researchContent" className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
             {research.map((data, id) => (
               <article key={id} className="relative">
@@ -213,7 +194,8 @@ export default function Main() {
         <div className="px-4 mx-auto max-w-screen-xl">
             <div className="flex items-center justify-between mb-8">
                 <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl dark:text-white">Başarılar</h1>
-                </div>
+                <Link prefetch={true} href="/achievements" rel="noopener noreferrer" className="inline-flex items-center font-medium underline underline-offset-4 text-black dark:text-gray-300 hover:no-underline">Tümünü gör</Link>
+            </div>
             <div id="achievementsContent" className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
             {ach.map((data, id) => (
               <article key={id} className="relative">
